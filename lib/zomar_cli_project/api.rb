cc34ec1d20d09b5294fe6c2ed800fad11f21668e
Zainab-Omar <zainab.alshaikhli01@gmail.com>
2020-08-19 14:32:37 -0500

    create a class called Edamam inside api.rb

--- lib/zomar_cli_project/api.rb
+++ lib/zomar_cli_project/api.rb
@@ -4,18 +4,27 @@ require 'uri'
 require 'net/http'
 require 'openssl'
 require 'pry'
-input = gets.strip
-url = URI("https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=1%20cup%20of%20#{input}")
 
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+class Edamam
 
-request = Net::HTTP::Get.new(url)
-request["x-rapidapi-host"] = 'edamam-edamam-nutrition-analysis.p.rapidapi.com'
-request["x-rapidapi-key"] = 'fb1f202508mshe6df39a6ec4a438p18ba5bjsn03d1a1af341b'
+ def get_url
+    input = gets.strip
+    url = URI("https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=1%20cup%20of%20#{input}")
+    
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-host"] = 'edamam-edamam-nutrition-analysis.p.rapidapi.com'
+    request["x-rapidapi-key"] = 'fb1f202508mshe6df39a6ec4a438p18ba5bjsn03d1a1af341b'
+    
+     response = http.request(request)
+     result= response.read_body
+     puts result
+  end
+end
+# show_result= Edamam.new.get_url
+# puts show_result
 
- response = http.request(request)
- puts response.read_body
-
- 
+    
