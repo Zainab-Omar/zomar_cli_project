f2ace8c581ca895b92fa6ac8b295689b2055eacd
Zainab-Omar <zainab.alshaikhli01@gmail.com>
2020-08-21 19:53:26 -0500

    working on api.rb made and array of edamam

--- lib/zomar_cli_project/api.rb
+++ lib/zomar_cli_project/api.rb
@@ -8,6 +8,7 @@ require 'pry'
 class Api
 
     def self.get_data
+      edamam=[]
       url = URI("https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=1%20large%20")
       http = Net::HTTP.new(url.host, url.port)
       http.use_ssl = true
@@ -15,15 +16,16 @@ class Api
       request = Net::HTTP::Get.new(url)
       request["x-rapidapi-host"] = 'edamam-edamam-nutrition-analysis.p.rapidapi.com'
       request["x-rapidapi-key"] = 'fb1f202508mshe6df39a6ec4a438p18ba5bjsn03d1a1af341b'
-
+      
       response = http.request(request)
       result= response.read_body
       parsed_data =JSON.parse(result)
       calories=parsed_data["calories"]
       dietLabels=parsed_data["dietLabels"]
-      healthLabel=parsed_data["healthLabels"]
+      healthLabels=parsed_data["healthLabels"]
       totalNutrients=parsed_data["totalNutrients"]
       totalDaily=parsed_data["totalDaily"]
+      edamam << {calories: calories, dietLabels: dietLabels, healthLabels: healthLabels, totalNutrients: totalNutrients, totalDaily: totalDaily}
     end
-     binding.pry
-end
+    #binding.pry
+ end  
